31516ec2c3920c4bdd9560baf03b6f4884c8beff
iiPing <dbx0001@gmail.com>
2015-04-09 15:08:14 +0800

    add some local changes with junit

---
+++ component/crypto/AssymX509Test/test-src/test/ph/gabijan/iiping/java/utils/crypto/Test.java
@@ -0,0 +1,28 @@
+package test.ph.gabijan.iiping.java.utils.crypto;
+
+import static org.junit.Assert.*;
+import ph.gabijan.iiping.java.utils.crypto.CryptoUtil;
+import ph.gabijan.iiping.java.utils.crypto.ICryptoUtilAssymRSA;
+
+public class Test {
+
+	@org.junit.Test
+	public void test() throws Throwable {
+
+		ICryptoUtilAssymRSA cryptoUtil = new CryptoUtil();
+
+		cryptoUtil
+				.loadPrivateKey("D:/usr/src/local/code.github/java-utils/AssymX509Test/priv_key.der");
+		cryptoUtil
+				.loadPublicKey("D:/usr/src/local/code.github/java-utils/AssymX509Test/pub_key.der");
+		
+		String testString = "this is a test string";
+		
+		String encStr = cryptoUtil.priEncStr(testString);
+		String decStr = cryptoUtil.pubDecStr(encStr);
+		
+		assertTrue("expected result on enc->dec for pri->pub", encStr.equals(decStr));
+
+	}
+
+}
